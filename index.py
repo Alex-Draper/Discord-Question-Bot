5e8bf4850a496d3349e23600deab77ed5dafc137
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-09 17:28:17 +0000

    saves input variables

--- index.py
+++ index.py
@@ -1,12 +1,66 @@
-from discord import Client, Intents, Embed
-from discord_slash import SlashCommand, SlashContext
+# from discord import Client, Intents, Embed
+# from discord_slash import SlashCommand, SlashContext
 
-bot = Client(intents=Intents.default())
-slash = SlashCommand(bot)
+# bot = Client(intents=Intents.default())
+# slash = SlashCommand(bot)
 
-@slash.slash(name="test")
-async def test(ctx: SlashContext):
-    embed = Embed(title="Embed Test")
-    await ctx.send(embed=embed)
+# @slash.slash(name="test")
+# async def test(ctx: SlashContext):
+#     embed = Embed(title="Embed Test")
+#     await ctx.send(embed=embed)
 
-bot.run("OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs")
+# bot.run("OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs")
+
+
+
+import requests
+
+
+url = "https://discord.com/api/v8/applications/927972609012928512/commands"
+
+# This is an example CHAT_INPUT or Slash Command, with a type of 1
+json = {
+    "name": "blep",
+    "type": 1,
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": True,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": False
+        }
+    ]
+}
+
+# For authorization, you can use either your bot token
+headers = {
+    "Authorization": "Bot OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs"
+}
+
+# or a client credentials token for your app with the applications.commands.update scope
+# headers = {
+#     "Authorization": "Bearer <my_credentials_token>"
+# }
+
+r = requests.post(url, headers=headers, json=json)
--- test.py
+++ test.py
@@ -24,60 +24,65 @@ async def on_ready():
 
 @bot.command()
 async def addQuestion(ctx, *questions: str):
-    print(questions)
+    count = 0
+    new_questions = []
+    for i in questions:
+        new_questions[count] = i
+        count += 1
+    # Add questions to database, right after all the previously asked questions so that the newly added one is picked next
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+# @bot.command()
+# async def add(ctx, left: int, right: int):
+#     """Adds two numbers together."""
+#     await ctx.send(left + right)
 
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split("d"))
-    except Exception:
-        await ctx.send("Format has to be in NdN!")
-        return
+# @bot.command()
+# async def roll(ctx, dice: str):
+#     """Rolls a dice in NdN format."""
+#     try:
+#         rolls, limit = map(int, dice.split("d"))
+#     except Exception:
+#         await ctx.send("Format has to be in NdN!")
+#         return
 
-    result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description="For when you wanna settle the score some other way")
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+#     result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
+#     await ctx.send(result)
 
 
-@bot.command()
-async def repeat(ctx, times: int, content="repeating..."):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
+# @bot.command(description="For when you wanna settle the score some other way")
+# async def choose(ctx, *choices: str):
+#     """Chooses between multiple choices."""
+#     await ctx.send(random.choice(choices))
 
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send("{0.name} joined in {0.joined_at}".format(member))
+# @bot.command()
+# async def repeat(ctx, times: int, content="repeating..."):
+#     """Repeats a message multiple times."""
+#     for i in range(times):
+#         await ctx.send(content)
 
 
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send("No, {0.subcommand_passed} is not cool".format(ctx))
+# @bot.command()
+# async def joined(ctx, member: discord.Member):
+#     """Says when a member joined."""
+#     await ctx.send("{0.name} joined in {0.joined_at}".format(member))
 
 
-@cool.command(name="bot")
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send("Yes, the bot is cool.")
+# @bot.group()
+# async def cool(ctx):
+#     """Says if a user is cool.
+#     In reality this just checks if a subcommand is being invoked.
+#     """
+#     if ctx.invoked_subcommand is None:
+#         await ctx.send("No, {0.subcommand_passed} is not cool".format(ctx))
+
+
+# @cool.command(name="bot")
+# async def _bot(ctx):
+#     """Is the bot cool?"""
+#     await ctx.send("Yes, the bot is cool.")
 
 
 bot.run("OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs")
