195c129e64f22f92653d3822536b82fa43d14b3f
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-09 19:08:18 +0000

    Update slash.py

--- slash.py
+++ slash.py
@@ -1,4 +1,5 @@
 from random import choices
+from typing_extensions import Required
 import discord
 from discord.ext import commands
 from discord import client
@@ -11,7 +12,7 @@ token = "OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs"
 
 @slash.slash(
     name="add",
-    description="Add question(s) to top of list.\"Question One?\" \"Question Two?\"",
+    description="Add question(s) to top of list.\"Question One\" \"Question Two\"",
     guild_ids=[845390667273338922],
     options=[
         create_option(
@@ -19,20 +20,36 @@ token = "OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs"
             description="Add to top of questions list?",
             required=True,
             option_type=3,
-            choices=[
-                create_choice(
-                    name="True",
-                    value="True"
-                ),
-                create_choice(
-                    name="False",
-                    value="False"
-                ),
-            ]
         )
     ]
 )
 async def _add(ctx:SlashContext, option:str):
     await ctx.send(option)
+
+# @slash.slash(
+#     name="add",
+#     description="Add question(s) to top of list.\"Question One?\" \"Question Two?\"",
+#     guild_ids=[845390667273338922],
+#     options=[
+#         create_option(
+#             name="option",
+#             description="Add to top of questions list?",
+#             required=True,
+#             option_type=3,
+#             choices=[
+#                 create_choice(
+#                     name="True",
+#                     value="True"
+#                 ),
+#                 create_choice(
+#                     name="False",
+#                     value="False"
+#                 ),
+#             ]
+#         )
+#     ]
+# )
+# async def _add(ctx:SlashContext, option:str):
+#     await ctx.send(option)
 
 client.run(token)
