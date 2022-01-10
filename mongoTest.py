e26486f1cb16260e4f0e9b7ae7ef494a187d119e
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-10 19:32:14 +0000

    Update mongoTest.py

--- mongoTest.py
+++ mongoTest.py
@@ -1,3 +1,4 @@
+import certifi
 from pymongo import MongoClient
 
 # pprint library is used to make the output look more pretty
@@ -5,7 +6,8 @@ from pprint import pprint
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient(
-    "mongodb+srv://admin:7pbKRmqoSyVmQTpXrEtdmo7xO@cluster0.z5ix9.mongodb.net/discordDB?retryWrites=true&w=majority"
+    "mongodb+srv://admin:7pbKRmqoSyVmQTpXrEtdmo7xO@cluster0.z5ix9.mongodb.net/discordDB?retryWrites=true&w=majority",
+    tlsCAFile=certifi.where()
 )
 dbAdmin = client.admin
 print("################")
@@ -13,9 +15,9 @@ db = client["discordDB"]
 collection = db["questions"]
 
 
-newPost = {"question": "What is your fav colour?"}
+# newPost = {"question": "What is your fav colour?"}
 
-collection.insert_one(newPost)
+# collection.insert_one(newPost)
 
 # Issue the serverStatus command and print the results
 # serverStatusResult = db.command("serverStatus")
