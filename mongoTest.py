803a192e86917fdb0ecb2241de1343e82e6b1869
alex.draper <alexdraper2020@outlook.com>
2022-01-10 19:21:47 +0000

    initial working insertion and retrieving from mongo

---
+++ mongoTest.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient(
+    "mongodb+srv://admin:7pbKRmqoSyVmQTpXrEtdmo7xO@cluster0.z5ix9.mongodb.net/discordDB?retryWrites=true&w=majority"
+)
+dbAdmin = client.admin
+print("################")
+db = client["discordDB"]
+collection = db["questions"]
+
+
+newPost = {"question": "What is your fav colour?"}
+
+collection.insert_one(newPost)
+
+# Issue the serverStatus command and print the results
+# serverStatusResult = db.command("serverStatus")
+
+# pprint(serverStatusResult)
+
+
+results = collection.find({"question": "What is your fav colour?"})
+
+for result in results:
+    print(result)
