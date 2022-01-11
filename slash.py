450aadb4f70c3b04146c23e3963f272951fc9080
alex.draper <alexdraper2020@outlook.com>
2022-01-11 22:19:44 +0000

    testing multiple async functions

--- slash.py
+++ slash.py
@@ -1,10 +1,12 @@
 from random import choices
 from typing_extensions import Required
 import discord
-from discord.ext import commands
+from discord import channel
+from discord.ext import commands, tasks
 from discord import client
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
+import time
 
 client = commands.Bot(command_prefix="!")
 slash = SlashCommand(client, sync_commands=True)
@@ -41,6 +43,15 @@ token = "OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs"
 )
 async def _add(ctx:SlashContext, list_location:str,questions:str):
     await ctx.send(list_location + questions)
+    
+@tasks.loop(seconds=2)
+async def loopThing():
+    channel = client.get_channel(845390668025298956)
+    if channel != None:
+        await channel.send('hello')
+    
+loopThing.start()
+client.run(token)
 
 # @slash.slash(
 #     name="add",
@@ -68,4 +79,3 @@ async def _add(ctx:SlashContext, list_location:str,questions:str):
 # async def _add(ctx:SlashContext, option:str):
 #     await ctx.send(option)
 
-client.run(token)
