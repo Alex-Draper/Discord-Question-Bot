751fdb05b6e4d501080356257e6a40fdf09b7d7e
Alex Draper <alexdraper2020@outlook.com>
2022-01-15 15:43:53 +0000

    created create post abstraction methods

--- MongoCRUD.py
+++ MongoCRUD.py
@@ -1,7 +1,5 @@
-from datetime import datetime
 import certifi
 from pymongo import MongoClient
-
 
 class MongoCRUD:
     def __init__(self):
@@ -11,6 +9,8 @@ class MongoCRUD:
         self.collection = self.db["questions"]
 
     def _createPost(self, items: dict):
+        if not isinstance(items, dict):
+            raise TypeError("items must be a dict")
         self.collection.insert_one(items)
 
     def _readPost(self, filters: dict):
@@ -22,10 +22,33 @@ class MongoCRUD:
     def _deletePost(self, collectionToDelete):
         self.collection.delete_one(collectionToDelete)
 
+    def addOneQuestionToDB(self, question : dict):
+        if not isinstance(question, dict):
+            raise TypeError("question must be of type dict")
+        self._createPost(question)
 
-if __name__ == "__main__":
+    def addManyQuestionsToDB(self, questions : list[dict]):
+        """ Test types first so we don't add any questions if a type is incorrect, helps with not adding repeat questions"""
 
-    mon = MongoCRUD()
+        if not all(isinstance(question, dict) for question in questions):
+            raise TypeError("all items in questions must be of type dict")
+        
+        for question in questions:
+            self.addOneQuestionToDB(question)
+
+
+
+# if __name__ == "__main__":
+
+#     mon = MongoCRUD()
+#     dq = DiscordQuestion("What is your fav color?", "PerryBot")
+#     mon.addOneQuestionToDB(dq.getAsDict())
+
+
+#     dq2 = DiscordQuestion("What is your fav animal?", "Serval")
+#     dq3 = DiscordQuestion("What is the best place in the world?", "Father CEO")
+
+#     mon.addManyQuestionsToDB([dq2.getAsDict(), dq3.getAsDict()])
     # mon.createPost(
     #     {
     #         "questionText": "What is your fav animal",
