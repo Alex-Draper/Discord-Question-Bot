deb67d4a14169c3c900c44478285c434d4338c4d
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-16 14:29:20 +0000

    splits arguments into list

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from discord import client
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
 import time
+from MongoCRUD import MongoCRUD
+from DiscordQuestion import DiscordQuestion
+import asyncio
 
 client = commands.Bot(command_prefix="!")
 slash = SlashCommand(client, sync_commands=True)
@@ -42,7 +45,19 @@ token = "OTI3OTcyNjA5MDEyOTI4NTEy.YdR_1g.6zMkvZ7wdaOs22F01LXg2jQGdIs"
     ]
 )
 async def _add(ctx:SlashContext, list_location:str,questions:str):
-    await ctx.send(list_location + questions)
+    await ctx.send("Questions added, "+str(ctx.author) +" <3")
+    print(ctx.author)
+    temp = [line for line in [line.strip() for line in questions.split("\"")] if line]
+    mon = MongoCRUD()
+    print(ctx.author)
+    for i in temp:
+        if list_location == "Top of List":
+            dq = DiscordQuestion(i, str(ctx.author), priority=1)
+        else:
+            dq = DiscordQuestion(i, str(ctx.author), priority=0)
+        mon.addOneQuestionToDB(dq.getAsDict())
+    print(ctx.author)
+    # await ctx.send("Questions added, "+str(ctx.author) +" <3")
     
 # @tasks.loop(seconds=2)
 # async def loopThing():
